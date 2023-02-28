1dac377d810163fba3e7325d9db65d3858b04292
tallez <allez@swift.ch>
2023-02-28 15:58:22 +0100

    first commit next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 
-import { AppProps } from "next/app"
 import "../styles/tailwind.scss"
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-
-export default MyApp
